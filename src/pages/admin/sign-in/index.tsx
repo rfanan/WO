940a4d078b5c1f1cfd92ec4f94580659375c4d4d
hira <51377578+hira0021@users.noreply.github.com>
2024-08-23 19:30:52 +0700

    add inputform component

--- src/pages/admin/sign-in/index.tsx
+++ src/pages/admin/sign-in/index.tsx
@@ -1,7 +1,41 @@
+import InputForm from "@/components/form/InputForm";
+import { COLOR } from "@/styles/color";
+import { Col, Form, Row } from "antd";
+
 export default function SignIn() {
   return (
-    <>
-      SignIn
-    </>
+    <div style={{
+      backgroundColor: COLOR.blue,
+      color: "black",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <div style={{
+        backgroundColor: "white",
+        minHeight: "500px",
+        width: "400px",
+        borderRadius: 4,
+        padding: 10
+      }}>
+        <Form layout={'vertical'}>
+          <Row gutter={16}>
+            <Col span={24}>
+              <InputForm
+                label={"Email"}
+                type="Input"
+              />
+            </Col>
+            <Col span={24}>
+              <InputForm
+                label={"Password"}
+                type="Password"
+              />
+            </Col>
+          </Row>
+        </Form>
+      </div>
+    </div>
   );
 }
--- src/util/constant.ts
+++ src/util/constant.ts
@@ -2,3 +2,9 @@ export const CONSTANT: any = {
     V1_VERSION: "v1",
     CLOUD_URL: process.env.CLOUD_URL + "/api/",
 }
+
+export enum PageState {
+    edit = "edit",
+    create = "create",
+    view = "view"
+}
